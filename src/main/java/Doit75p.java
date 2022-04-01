bcc211ff2385a7367e5e997abef0bf5f2117212f
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-04-01 10:10:26 +0900

    Initial commit

---
+++ src/main/java/Doit75p.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+public class Doit75p {
+    public static void main(String[] args) {
+
+        int gameScore = 150;
+
+        int lastScore1 = ++gameScore;
+        System.out.println(lastScore1);
+
+        int lastScore2 = --gameScore;
+        System.out.println(lastScore2);
+
+    }
+}
